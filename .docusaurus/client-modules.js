e82f0a138767f6ba2a72e4127b0fe8e12555e8cd
Aris Ripandi <ar.is@outlook.com>
2021-10-08 12:37:18 +0700

    Remove .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/ariss/Sandbox/headunit/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/ariss/Sandbox/headunit/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/ariss/Sandbox/headunit/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/ariss/Sandbox/headunit/src/css/custom.css"),
-];
