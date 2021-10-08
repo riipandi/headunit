f1047beb7e0b6029e0710bde1b70047fe35f69d3
Aris Ripandi <ar.is@outlook.com>
2021-10-08 12:36:29 +0700

    Add dante editor

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/ariss/Sandbox/headunit/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/ariss/Sandbox/headunit/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/ariss/Sandbox/headunit/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/ariss/Sandbox/headunit/src/css/custom.css"),
+];
